9a1b41d030ed83bc40144685aca77d42c7923e9c
Flargy <marlun1996@hotmail.com>
2018-03-31 20:56:15 +0200

    Fixed stuff
    Forgot to add @Override

--- src/Devices.java
+++ src/Devices.java
@@ -14,7 +14,8 @@ public class Devices extends Valuables {
 	public double calcValue () {
 		return ((condition/10) * buyoutPrice);
 	}
-
+	
+	@Override
 	public String toString() {
 		return "Device: " + getName() + "condition: + condition + " + "value: " + getValue();
 	}
